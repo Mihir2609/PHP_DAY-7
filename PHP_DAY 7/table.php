bf2e1f5f47437dba0f91ca1c46eadee132f458b0
Mihir2609 <84737914+Mihir2609@users.noreply.github.com>
2021-06-03 22:57:31 +0530

    Update table.php

--- PHP_DAY 7/table.php
+++ PHP_DAY 7/table.php
@@ -1,11 +1,12 @@
 <?php
 //DB connection
-$connection = mysqli_connect("localhost","root","","db_internship");
+$connection = mysqli_connect("localhost","root","","db_phpinternship");
  //Query
 $q = mysqli_query($connection,"select * from tbl_user") or die(mysqli_error($connection));
 
 echo "<table border='1'>";
 echo "<tr>";
+echo "<th>Id</th>";
 echo "<th>Name</th>";
 echo "<th>Gender</th>";
 echo "<th>Mobile</th>";
@@ -14,12 +15,13 @@ echo "</tr>";
 while($row = mysqli_fetch_array($q)){
  
     echo "<tr>";
+    echo "<td>{$row['user_id']}</td>";
     echo "<td>{$row['user_name']}</td>";
-      echo "<td>{$row['user_gender']}</td>";
-        echo "<td>{$row['user_mobile']}</td>";
-       echo "<td><a href='Delete.php?deletedname={$row['user_name']}'>Delete</a></td>"; 
-        echo "</tr>";
+    echo "<td>{$row['user_gender']}</td>";
+    echo "<td>{$row['user_mobile']}</td>";
+    echo "<td><a href='delete.php?deleteid={$row['user_id']}'>Delete</a></td>"; 
+    echo "</tr>";
 }
 echo "</table>";
-echo"<a href='connection.php'>Add Record</a>";
+echo"<a href='insert_record.php'>Add Record</a>";
 ?>
